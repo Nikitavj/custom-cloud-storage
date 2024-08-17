52211a32a5c620101ab5476e27a5286e52ead3d2
Nikita <nk.dsn.nv@gmail.com>
2024-08-17 22:19:03 +0300

    feat: create PathDirectoryService.java

--- src/main/java/org/nikita/spingproject/filestorage/directory/PathDirectoryService.java
+++ src/main/java/org/nikita/spingproject/filestorage/directory/PathDirectoryService.java
@@ -1,61 +1,15 @@
 package org.nikita.spingproject.filestorage.directory;
 
-import jakarta.persistence.EntityNotFoundException;
-import org.apache.commons.lang3.StringUtils;
-import org.nikita.spingproject.filestorage.account.User;
-import org.nikita.spingproject.filestorage.account.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface PathDirectoryService {
+    String rootPathForUser(String userName);
 
-@Service
-public class PathDirectoryService {
-    @Autowired
-    private UserRepository userRepository;
+    String absolutPath(String relativePath, String userName);
 
-    public String rootPathForUser(String userName) {
-        User user = userRepository.findUserByEmail(userName)
-                .orElseThrow(() -> new EntityNotFoundException("User " + userName + "not exist"));
-        return String.format("user-%s-files", user.getId());
-    }
+    String absolutePathNewDir(String currentPath, String name, String userName);
 
-    public String absolutPath(String relativePath, String userName) {
-        String rootPath = rootPathForUser(userName);
-        if (relativePath == null) {
-            return String.format("%s", rootPath);
-        } else {
-            return String.format("%s/%s",
-                    rootPath,
-                    relativePath);
-        }
-    }
+    String relativePath(String path, String name);
 
-    public String absolutePathNewDir(String currentPath, String name, String userName) {
-        String rootPath = rootPathForUser(userName);
-        if (currentPath.isBlank()) {
-            return String.format("%s/%s", rootPath, name);
-        } else {
-            return String.format("%s/%s/%s", rootPath, currentPath, name);
-        }
-    }
+    String renameAbsolutePath(String previousAbsolutePath, String newName);
 
-    public String relativePath(String path, String name) {
-        if (path.isBlank()) {
-            return name;
-        } else {
-            return String.format("%s/%s", path, name);
-        }
-    }
-
-    public String renameAbsolutePath(String previousAbsolutePath, String newName) {
-        String path = StringUtils.removeEnd(previousAbsolutePath,"/");
-        return StringUtils.substringBeforeLast(path, "/") + "/" + newName;
-    }
-
-    public String renameRelativePath(String previousPath, String newName) {
-        if(previousPath.contains("/")) {
-            return StringUtils.substringBeforeLast(previousPath, "/") + "/" + newName;
-        } else {
-            return newName;
-        }
-    }
+    String renameRelativePath(String previousPath, String newName);
 }
