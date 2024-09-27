5acd3153590abf735f96ec3237b76d46d38e15a0
Nikita <nk.dsn.nv@gmail.com>
2024-09-27 23:10:59 +0300

    refactor: mapping of files and directories is carried out in ToObjectStorageMapper

---
+++ src/main/java/org/nikita/spingproject/filestorage/utils/ToObjectStorageMapper.java
@@ -0,0 +1,27 @@
+package org.nikita.spingproject.filestorage.utils;
+
+import org.apache.commons.io.FileUtils;
+import org.nikita.spingproject.filestorage.commons.ObjectStorageDto;
+import org.nikita.spingproject.filestorage.directory.Directory;
+import org.nikita.spingproject.filestorage.file.File;
+
+public class ToObjectStorageMapper {
+    public static ObjectStorageDto mapFileToObjStorage(File file) {
+        return ObjectStorageDto.builder()
+                .name(file.getName())
+                .relativePath(file.getPath())
+                .date(DateFormatUtil.format(file.getDate()))
+                .size(FileUtils.byteCountToDisplaySize(file.getSize()))
+                .isDir(false)
+                .build();
+    }
+
+    public static ObjectStorageDto mapDirToObjStorage(Directory dir) {
+        return ObjectStorageDto.builder()
+                .name(dir.getName())
+                .relativePath(dir.getPath())
+                .date(DateFormatUtil.format(dir.getDate()))
+                .isDir(true)
+                .build();
+    }
+}
